b5f889038609c83ad23e0ae7443605fac716b0ad
diredocks <26994007+diredocks@users.noreply.github.com>
2026-09-14 10:01:09 +0800

    refactor

--- src/packet/builder.c
+++ src/packet/builder.c
@@ -175,9 +175,6 @@ PacketBuilder *packet_builder_get() {
 }
 
 void packet_builder_free() {
-  if (g_builder == NULL)
-    return;
-
   free_ptr(&((PacketBuilderPriv *)g_builder->priv)->md5_seed);
   free_ptr(&g_builder->priv);
   free_ptr(&g_builder);
